3ac5f5cb9c6969ae14603839fa68958453bc5180
Imem Ayachi <imem.playz@gmail.com>
2025-02-08 00:17:16 +0100

    initial commit

--- src/main/java/tn/esprit/atlas/AtlasApplication.java
+++ src/main/java/tn/esprit/atlas/AtlasApplication.java
@@ -13,25 +13,21 @@ import java.io.InputStream;
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(AtlasApplication.class.getResource("/tn/esprit/atlas/views/main-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 800, 600); // Set initial size
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
         stage.setTitle("ATLAS");
 
-        // Load the icon image
         InputStream inputStream = getClass().getResourceAsStream("/tn/esprit/atlas/assets/ATLAS_LOGO.png");
         if (inputStream == null) {
             System.err.println("Icon file not found! Check the path.");
         } else {
             Image icon = new Image(inputStream);
-            stage.getIcons().add(icon); // Set the icon for the stage
+            stage.getIcons().add(icon);
         }
 
-        // Set the scene
         stage.setScene(scene);
 
-        // Maximize the window (like Google Chrome)
-        stage.setMaximized(true); // This keeps the title bar, close button, and icon visible
+        stage.setMaximized(true);
 
-        // Show the stage
         stage.show();
     }
 
