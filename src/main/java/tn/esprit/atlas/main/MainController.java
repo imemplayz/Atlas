2c7ec27e2f992412e49bab31869264ff4f1bd97e
Imem Ayachi <imem.playz@gmail.com>
2025-02-15 13:31:21 +0100

    Updated MainController

--- src/main/java/tn/esprit/atlas/main/MainController.java
+++ src/main/java/tn/esprit/atlas/main/MainController.java
@@ -13,6 +13,12 @@ public class MainController {
 
     @FXML
     private Button switchToSignInButton;
+    @FXML
+    private Button switchToReservationButton;
+    @FXML
+    private Button switchToVolsButton;
+    @FXML
+    private Button switchToAirlinesButton;
 
     @FXML
     private void switchToSignIn() throws IOException {
@@ -24,5 +30,41 @@ public class MainController {
 
         // Set the new root to the existing scene
         currentScene.setRoot(signInRoot);
+    }
+
+    @FXML
+    private void switchToReservation() throws IOException {
+        // Load the sign-in view
+        Parent reservationRoot = FXMLLoader.load(getClass().getResource("/tn/esprit/atlas/views/reservation-view.fxml"));
+
+        // Get the current scene
+        Scene currentScene = switchToReservationButton.getScene();
+
+        // Set the new root to the existing scene
+        currentScene.setRoot(reservationRoot);
+    }
+
+    @FXML
+    private void switchToVols() throws IOException {
+        // Load the sign-in view
+        Parent volsRoot = FXMLLoader.load(getClass().getResource("/tn/esprit/atlas/views/vols-view.fxml"));
+
+        // Get the current scene
+        Scene currentScene = switchToVolsButton.getScene();
+
+        // Set the new root to the existing scene
+        currentScene.setRoot(volsRoot);
+    }
+
+    @FXML
+    private void switchToAirlines() throws IOException {
+        // Load the sign-in view
+        Parent airlinesRoot = FXMLLoader.load(getClass().getResource("/tn/esprit/atlas/views/airlines-view.fxml"));
+
+        // Get the current scene
+        Scene currentScene = switchToAirlinesButton.getScene();
+
+        // Set the new root to the existing scene
+        currentScene.setRoot(airlinesRoot);
     }
 }
